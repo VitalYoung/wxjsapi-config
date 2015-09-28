aec4e0aeb9cc5c34caab0463d2a4266557b6a9d5
Nick.zhu <nick.zhu@icitymoble.com>
2015-09-28 18:03:14 +0800

    class WechatConfig

--- lib/wxjsapi/config.rb
+++ lib/wxjsapi/config.rb
@@ -2,6 +2,98 @@ require "wxjsapi/config/version"
 
 module Wxjsapi
   module Config
-    # Your code goes here...
+    class WechatConfig
+      attr_accessor :debug, :appId, :appSecret, :timestamp, :nonceStr, :signature, :jsApiList
+      def initialize appId, appSecret, jsApiList
+        #全局缓存access_token
+        $access_token = Hash.new
+        $access_token['access_token'] = ''
+        $access_token['expires_in'] = Time.new
+        #全局缓存jsapi_ticket
+        $jsapi_ticket = Hash.new
+        $jsapi_ticket['jsapi_ticket'] = ''
+        $jsapi_ticket['expires_in'] = Time.new
+
+        @appId = appId
+        @appSecret = appSecret
+        if jsApiList.present?
+          @jsApiList = jsApiList
+        else
+          @jsApiList = "onMenuShareTimeline,onMenuShareAppMessage,onMenuShareQQ,onMenuShareWeibo,startRecord,stopRecord,onVoiceRecordEnd,playVoice,pauseVoice,stopVoice,onVoicePlayEnd,uploadVoice,downloadVoice,chooseImage,previewImage,uploadImage,downloadImage,translateVoice,getNetworkType,openLocation,getLocation,hideOptionMenu,showOptionMenu,hideMenuItems,showMenuItems,hideAllNonBaseMenuItem,showAllNonBaseMenuItem,closeWindow,scanQRCode,chooseWXPay,openProductSpecificView,addCard,chooseCard,openCard"
+        end
+        self.debug = 'fales'
+      end
+
+      def wxJsSDKSign url
+        uuid = UUID.new
+        self.timestamp = Time.new.to_i.to_s
+        self.nonceStr = uuid.generate
+        ticket = self.wxGetJsAPITicket
+        puts 'ticket:' + ticket.to_s
+        if ticket.blank?
+        return false
+        end
+
+        str = 'jsapi_ticket=' + ticket.to_s + '&noncestr=' + self.nonceStr.to_s + '&timestamp=' + self.timestamp + '&url=' + url.to_s
+        puts str
+        self.signature = Digest::SHA1.hexdigest(str)
+      end
+
+      #获取调用微信JS接口的临时票据 jsapi_ticket
+      def wxGetJsAPITicket
+        today = Time.new
+        ticket_ex = $jsapi_ticket['expires_in']
+        if today < ticket_ex
+          $jsapi_ticket['jsapi_ticket']
+        else
+          token_ex = $access_token['expires_in']
+          if today > token_ex
+            token = self.wxGetAccessToken
+            if token.blank?
+              return nil
+            end
+          else
+            token = $access_token['access_token']
+          end
+          puts 'token:' + token.to_s
+          url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?type=jsapi&' + 'access_token=' + token.to_s
+          response = RestClient.get url
+          if response.code == 200
+            result = ActiveSupport::JSON.decode(response)
+            puts 'ticket result:' + result.to_s
+            ticket = result['ticket']
+            $jsapi_ticket['jsapi_ticket'] = ticket
+            $jsapi_ticket['expires_in'] = Time.new + result['expires_in'].to_i
+          return ticket
+          else
+            return nil
+          end
+        end
+      end
+
+      #获取公众号的全局唯一票据access_token
+      def wxGetAccessToken
+        today = Time.new
+        token_ex = $access_token['expires_in']
+        puts today<token_ex
+        if today < token_ex
+          $access_token['access_token']
+        else
+          url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+ self.appId.to_s + '&secret=' + self.appSecret.to_s
+          puts 'url:' + url.to_s
+          response = RestClient.get url
+          if response.code == 200
+            result = ActiveSupport::JSON.decode(response)
+            puts 'token result:' + result.to_s
+            token = result['access_token']
+            $access_token['access_token'] = token
+            $access_token['expires_in'] = Time.new + result['expires_in'].to_i
+          return token
+          else
+            return nil
+          end
+        end
+      end
+    end
   end
 end
--- wxjsapi-config.gemspec
+++ wxjsapi-config.gemspec
@@ -9,14 +9,21 @@ Gem::Specification.new do |spec|
   spec.authors       = ["Nick.zhu"]
   spec.email         = ["nick.zhu@icitymoble.com"]
 
-  spec.summary       = %q{TODO: Write a short summary, because Rubygems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = %q{微信JS-SDK wx.config 配置参数}
+  spec.description   = %q{所有需要使用JS-SDK的页面必须先注入配置信息，否则将无法调用（同一个url仅需调用一次，对于变化url的SPA的web app可在每次url变化时进行调用,目前Android微信客户端不支持pushState的H5新特性，所以使用pushState来实现web app的页面会导致签名失败，此问题会在Android6.2中修复）。wx.config({
+      debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+      appId: '', // 必填，公众号的唯一标识
+      timestamp: , // 必填，生成签名的时间戳
+      nonceStr: '', // 必填，生成签名的随机串
+      signature: '',// 必填，签名，见附录1
+      jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
+  });}
+  spec.homepage      = "https://github.com/VitalYoung/wxjsapi-config"
 
   # Prevent pushing this gem to RubyGems.org by setting 'allowed_push_host', or
   # delete this section to allow pushing this gem to any host.
   if spec.respond_to?(:metadata)
-    spec.metadata['allowed_push_host'] = "TODO: Set to 'http://mygemserver.com'"
+    spec.metadata['allowed_push_host'] = "Set to 'http://mygemserver.com'"
   else
     raise "RubyGems 2.0 or newer is required to protect against public gem pushes."
   end
